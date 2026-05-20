1b8860e0b441a675cecf6697a44cea98eed3eb51
fate <dmitryifate@gmail.com>
2026-05-20 21:43:50 +0200

    storage load_db + save_db

---
+++ storage.py
@@ -0,0 +1,16 @@
+import json
+def save_db(data):
+        with ("database.json", "w") as f:
+            return json.dump(data, f)
+def load_db():
+    try:
+        with open("database.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        default = {
+            "balance": 0.0,
+            "transactions": [],
+            "investments": []
+        }
+        save_db(default)
+        return default
